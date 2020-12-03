3fa2779bc1f785388ff6331edbf4d37e09bdaa33
Mati Santillan <Matias1.santillan@gmail.com>
2020-12-03 10:19:56 -0300

    Update app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import{BrowserRouter, Switch, Route} from 'react-router-dom'
-import SearchResultPage from '../containers/SearchResultPage.jsx'
+import SearchResultPage from '../containers/searchResultPage.jsx'
 import Artist from '../containers/Artist.jsx'
 import Home from '../containers/HomePage.jsx'
 
